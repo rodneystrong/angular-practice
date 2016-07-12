6276c322365e22505e347f5f0aee7e3fc8284b0b
aleeex <akang2@gmail.com>
2016-07-12 16:43:12 -0600

    added code for the ctrl to grab data from the service

--- MainCtrl.js
+++ MainCtrl.js
@@ -1,5 +1,10 @@
 angular
   .module('myApp')
-  .controller('myCtrl', function($scope) {
+  .controller('myCtrl', function($scope, groceryService) {
+
+    //the groceries name here is arbitrary. it's like
+    //attaching a property to the $scope
+    $scope.groceries = groceryService.getGroceries();
+    console.log($scope.groceries);
 
   });
--- MainServ.js
+++ MainServ.js
@@ -6,6 +6,11 @@ angular
         name: 'banana',
         qty: 4,
         price: 3
+      },
+      {
+        name: 'apple',
+        qty: 2,
+        price: 10
       }
     ];
     this.getGroceries = function() {
